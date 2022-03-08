f45e8e68e901a88c01e9d2ec4031661ac1bcc94c
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-03-08 13:27:29 -0500

    add todo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,26 @@ import { Box } from '@mantine/core'
 import './App.css'
 
 import useSWR from 'swr'
+import AddTodo from './components/AddTodo'
 const fetcher = (url:string) => fetch(`${ENDPOINT}/${url}`).then(r => r.json())
 export const ENDPOINT = 'http://localhost:4000'
 
-
+export interface Todo {
+  id: number
+  title: string
+  body: string
+  done: boolean;
+}
 
 function App() {
-  const {data,mutate} = useSWR('api/todos', fetcher)
+  const {data,mutate} = useSWR<Todo[]>('api/todos', fetcher)
 
 
   return (
     <Box>
       {JSON.stringify(data)}
+      <AddTodo mutate={mutate}/>
+
     </Box>
     
   )
---
+++ client/src/components/AddTodo.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import { useForm } from '@mantine/hooks';
+import { Button, Modal, Group, TextInput, Textarea } from '@mantine/core';
+import { ENDPOINT, Todo } from '../App';
+import { KeyedMutator } from 'swr';
+
+function AddTodo({mutate}: {mutate:KeyedMutator<Todo[]>}) {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm({
+    initialValues: {
+      title: '',
+      body: '',
+    },
+  });
+
+  async  function createTodo(values:{title:string, body:string}) {
+    const updated = await fetch(`${ENDPOINT}/api/todos`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(values)
+    }).then(response => response.json())
+    mutate(updated)
+    form.reset()
+    setOpen(false)
+
+  }
+  return (
+    <>
+      <Modal opened={open} onClose={() => setOpen(false)} title='create todo'>
+        Text{' '}
+      </Modal>
+      <form onSubmit={form.onSubmit(createTodo)}>
+        <TextInput
+          required
+          mb={12}
+          label='Todo'
+          placeholder='What do you want to do?'
+          {...form.getInputProps('title')}
+        />
+
+        <Textarea
+          required
+          mb={12}
+          label='body'
+          placeholder='Tell me more...'
+          {...form.getInputProps('body')}
+        />
+      </form>
+      <Group position='center'>
+        <Button fullWidth mb={12} onClick={() => setOpen(true)}>
+          Add TODO
+        </Button>
+      </Group>
+    </>
+  );
+}
+
+export default AddTodo;
