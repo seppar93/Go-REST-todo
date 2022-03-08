a70945ade4d443ceafde47dc39bcfbb87b52c520
Sepehr Parirokh <sepehrparirokh@gmail.com>
2022-03-08 13:34:41 -0500

    update todo

--- client/src/components/AddTodo.tsx
+++ client/src/components/AddTodo.tsx
@@ -1,58 +1,59 @@
-import { useState } from 'react';
-import { useForm } from '@mantine/hooks';
-import { Button, Modal, Group, TextInput, Textarea } from '@mantine/core';
-import { ENDPOINT, Todo } from '../App';
-import { KeyedMutator } from 'swr';
+import { useState } from "react";
+import { useForm } from "@mantine/hooks";
+import { Button, Modal, Group, TextInput, Textarea } from "@mantine/core";
+import { ENDPOINT, Todo } from "../App";
+import { KeyedMutator } from "swr";
 
-function AddTodo({mutate}: {mutate:KeyedMutator<Todo[]>}) {
+function AddTodo({ mutate }: { mutate: KeyedMutator<Todo[]> }) {
   const [open, setOpen] = useState(false);
 
   const form = useForm({
     initialValues: {
-      title: '',
-      body: '',
+      title: "",
+      body: "",
     },
   });
 
-  async  function createTodo(values:{title:string, body:string}) {
+  async function createTodo(values: { title: string; body: string }) {
     const updated = await fetch(`${ENDPOINT}/api/todos`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(values)
-    }).then(response => response.json())
-    mutate(updated)
-    form.reset()
-    setOpen(false)
+      body: JSON.stringify(values),
+    }).then((r) => r.json());
+
+    mutate(updated);
+    form.reset();
+    setOpen(false);
   }
 
-  
   return (
     <>
-      <Modal opened={open} onClose={() => setOpen(false)} title='create todo'>
-        Text{' '}
-      </Modal>
-      <form onSubmit={form.onSubmit(createTodo)}>
-        <TextInput
-          required
-          mb={12}
-          label='Todo'
-          placeholder='What do you want to do?'
-          {...form.getInputProps('title')}
-        />
+      <Modal opened={open} onClose={() => setOpen(false)} title="Create todo">
+        <form onSubmit={form.onSubmit(createTodo)}>
+          <TextInput
+            required
+            mb={12}
+            label="Todo"
+            placeholder="What do you want to do?"
+            {...form.getInputProps("title")}
+          />
+          <Textarea
+            required
+            mb={12}
+            label="Body"
+            placeholder="Tell me more..."
+            {...form.getInputProps("body")}
+          />
 
-        <Textarea
-          required
-          mb={12}
-          label='body'
-          placeholder='Tell me more...'
-          {...form.getInputProps('body')}
-        />
-      </form>
-      <Group position='center'>
+          <Button type="submit">Create todo</Button>
+        </form>
+      </Modal>
+
+      <Group position="center">
         <Button fullWidth mb={12} onClick={() => setOpen(true)}>
-          Add TODO
+          ADD TODO
         </Button>
       </Group>
     </>
